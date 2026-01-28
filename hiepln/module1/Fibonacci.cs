ec6f92a59c1948f4eab8d73c6b48de43c303d702
LaHiep2004 <lahiep2k4@gmail.com>
2026-01-28 22:10:50 +0700

    Implement Fibonacci using recursion

---
+++ hiepln/module1/Fibonacci.cs
@@ -0,0 +1,37 @@
+﻿public class Fibonacci : IFibonacci
+{
+    public int Fun(int n, bool flag)
+    {
+        if (n < 0)
+            throw new ArgumentException("n must be >= 0");
+
+        return flag ? FibonacciRecursive(n) : FibonacciLoop(n);
+    }
+
+    // Đệ quy
+    private int FibonacciRecursive(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+    }
+
+    // Vòng lặp
+    private int FibonacciLoop(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        int a = 0, b = 1, c = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+}
