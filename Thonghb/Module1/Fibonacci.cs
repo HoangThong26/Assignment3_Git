f0016d694ba39490aeeabd10aaa47bb26cdacfab
HoangThong26 <hbt2k4@gmail.com>
2026-01-28 19:46:12 +0700

    [impl2] Implement Fibonacci loop when flag = false

--- Thonghb/Module1/Fibonacci.cs
+++ Thonghb/Module1/Fibonacci.cs
@@ -4,45 +4,20 @@ namespace hieuht24.module1
     {
         public int Fibonacci(int n, bool flag)
         {
-            if (n < 0)
-                throw new ArgumentException("n must be >= 0");
-
             if (flag)
-            {
-                // Recursive
-                return FibonacciRecursive(n);
-            }
-            else
-            {
-                // Iterative
-                return FibonacciIterative(n);
-            }
-        }
+                throw new ArgumentException("Flag must be false for impl2");
 
-        private int FibonacciRecursive(int n)
-        {
-            if (n == 0) return 0;
-            if (n == 1) return 1;
+            if (n <= 1) return n;
 
-            return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
-        }
-
-        private int FibonacciIterative(int n)
-        {
-            if (n == 0) return 0;
-            if (n == 1) return 1;
-
-            int a = 0;
-            int b = 1;
-
+            int a = 0, b = 1;
             for (int i = 2; i <= n; i++)
             {
                 int temp = a + b;
                 a = b;
                 b = temp;
             }
-
             return b;
         }
+
     }
 }
