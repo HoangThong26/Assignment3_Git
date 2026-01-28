3a34caf9a3b7144b7561d551e1fd6264345f764b
LaHiep2004 <lahiep2k4@gmail.com>
2026-01-28 22:12:39 +0700

    Implement Fibonacci using for loop

---
+++ hiepln/module1/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Fibonacci : IFibonacci
+{
+    public int Fun(int n, bool flag)
+    {
+        if (flag)
+            throw new InvalidOperationException("Impl2 chỉ xử lý flag = false");
+
+        return Loop(n);
+    }
+
+    private int Loop(int n)
+    {
+        if (n <= 1) return n;
+
+        int a = 0, b = 1, c = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
